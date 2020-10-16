b2cfceb8117c29877a6428079b6149ec08dbea93
PrasannaObuliraj <press1998@gmail.com>
2020-10-16 09:35:45 +0530

    Added die rolling functionality

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,13 @@
 public class SnakeAndLadder
 {
+	function dieRoll
+	{
+		int roll= (int) (Math.floor(Math.random()*11)%6)+1;
+		return roll;
+	}
 	public static void main(String[] args)
 	{
 		int position=0;
+		int roll=dieRoll;
 	}
 }
