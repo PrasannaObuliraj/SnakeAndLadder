dee68cdf0f016268e11d71e7550a7be1726334ae
PrasannaObuliraj <press1998@gmail.com>
2020-10-16 10:44:23 +0530

    Fixed the winning position as 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,24 +5,46 @@ public class SnakeAndLadder
 		int roll= (int) (Math.floor(Math.random()*11)%6)+1;
 		return roll;
 	}
+	public static int forwardMove(int position,int roll)
+	{
+		if(position+roll>100)
+		{
+			System.out.println("You cant move past 100. Make another move");
+			return position;
+		}
+		else
+		{
+			position+=roll;
+			return position;
+		}
+	}
 	public static void main(String[] args)
 	{
 		int position=0;
-		int roll=dieRoll();
-		position+=roll;
-		int option = (int) (Math.floor(Math.random()*3)%3);
-		switch(option)
+		while(position<100)
 		{
-			case 0:
-			System.out.println("You are at " + position + " and there is no snake or ladder");
-			break;
-			case 1:
-			position+=roll;
-			System.out.println("You landed on a ladder and you move forward " + roll + " spaces and now you are at " + position);
-			break;
-			case 2:
-			position-=roll;
-			System.out.println("You landed on a snake and you move backward " + roll + " spaces and now you are at " + position);
+			int roll=dieRoll();
+			position=forwardMove(position,roll);
+			int option = (int) (Math.floor(Math.random()*3)%3);
+			switch(option)
+			{
+				case 0:
+				if(position<100)
+					System.out.println("You are at " + position + " and there is no snake or ladder");
+				break;
+				case 1:
+				position=forwardMove(position,roll);
+				if(position<100)
+					System.out.println("You landed on a ladder and you move forward " + roll + " spaces and now you are at " + position);
+				break;
+				case 2:
+				if(position<100)
+				{
+					position-=roll;
+					System.out.println("You landed on a snake and you move backward " + roll + " spaces and now you are at " + position);
+				}
+			}
 		}
+		System.out.println("You have reached 100, you have won");
 	}
 }
