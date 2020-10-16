e691e0c51840974596c3decfa2a003ac689926f9
PrasannaObuliraj <press1998@gmail.com>
2020-10-16 10:21:32 +0530

    Added functionality for snake ladder and no play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
 public class SnakeAndLadder
 {
-	function dieRoll
+	public static int dieRoll()
 	{
 		int roll= (int) (Math.floor(Math.random()*11)%6)+1;
 		return roll;
@@ -8,6 +8,21 @@ public class SnakeAndLadder
 	public static void main(String[] args)
 	{
 		int position=0;
-		int roll=dieRoll;
+		int roll=dieRoll();
+		position+=roll;
+		int option = (int) (Math.floor(Math.random()*3)%3);
+		switch(option)
+		{
+			case 0:
+			System.out.println("You are at " + position + " and there is no snake or ladder");
+			break;
+			case 1:
+			position+=roll;
+			System.out.println("You landed on a ladder and you move forward " + roll + " spaces and now you are at " + position);
+			break;
+			case 2:
+			position-=roll;
+			System.out.println("You landed on a snake and you move backward " + roll + " spaces and now you are at " + position);
+		}
 	}
 }
