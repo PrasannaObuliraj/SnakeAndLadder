8fb65e7e2e40192c314cf1b399b65b2b7e60807e
PrasannaObuliraj <press1998@gmail.com>
2020-10-16 11:20:58 +0530

    Reported roll on dice position after each roll and turns correctly

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,7 +9,7 @@ public class SnakeAndLadder
 	{
 		if(position+roll>100)
 		{
-			System.out.println("You cant move past 100. Make another move");
+			System.out.println("You cant move past 100. Make another roll");
 			return position;
 		}
 		else
@@ -21,9 +21,12 @@ public class SnakeAndLadder
 	public static void main(String[] args)
 	{
 		int position=0;
+		int turn=0;
 		while(position<100)
 		{
 			int roll=dieRoll();
+			System.out.println("Your role is " + roll);
+			turn++;
 			position=forwardMove(position,roll);
 			int option = (int) (Math.floor(Math.random()*3)%3);
 			switch(option)
@@ -31,11 +34,20 @@ public class SnakeAndLadder
 				case 0:
 				if(position<100)
 					System.out.println("You are at " + position + " and there is no snake or ladder");
+				else
+					System.out.println("You are at 100");
 				break;
 				case 1:
-				position=forwardMove(position,roll);
 				if(position<100)
-					System.out.println("You landed on a ladder and you move forward " + roll + " spaces and now you are at " + position);
+				{
+					position=forwardMove(position,roll);
+					if(position<=100)
+						System.out.println("You landed on a ladder and you move forward " + roll + " spaces and now you are at " + position);
+					else
+						System.out.println("You are at " + position);
+				}
+				else
+					System.out.println("You are at 100");
 				break;
 				case 2:
 				if(position<100)
@@ -43,8 +55,10 @@ public class SnakeAndLadder
 					position-=roll;
 					System.out.println("You landed on a snake and you move backward " + roll + " spaces and now you are at " + position);
 				}
+				else
+					System.out.println("You are at 100");
 			}
 		}
-		System.out.println("You have reached 100, you have won");
+		System.out.println("You have reached 100, you have won in " + turn + " turns");
 	}
 }
